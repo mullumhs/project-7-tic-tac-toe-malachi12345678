e64321e150f63dd43d4ff9b30f873d6bdd433178
malachi12345678 <140124190+malachi12345678@users.noreply.github.com>
2024-04-02 15:00:16 +1100

    fdsgdfgdgerdg

--- connect-four.py
+++ connect-four.py
@@ -1,5 +1,28 @@
 def main():
-    # Your code goes here
+    board = initialiseBoard()
+    displayBoard(board)
+    
+def initialiseBoard():
+    board = []
+    for _ in range(6):
+        row = []
+        for _ in range(7):
+            row.append('-')
+        board.append(row)
+    return board
+    
+def displayBoard(board):
+    for row in board:
+        for cell in row:
+            print(cell, end=' ')
+        print()
+    print()
+
+def playerTurn():
+    currentPlayer = 7/12
+
+
+
 
 if __name__ == "__main__":
     main()
