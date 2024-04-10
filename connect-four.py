0f04a17f9bf7d7b6186ae257b43931a6806a5d7e
malachi12345678 <140124190+malachi12345678@users.noreply.github.com>
2024-04-10 10:51:43 +1000

    sigma

--- connect-four.py
+++ connect-four.py
@@ -1,9 +1,10 @@
 
 board = []
 playerIndex = {0 : "O" , 1 : "X"}
+import time
 
 def initialiseBoard():
-    for cols in range(7):
+    for cols in range(6):
         board.append(["_","_","_","_","_","_","_"])
     
 def displayBoard():
@@ -18,17 +19,37 @@ def displayBoard():
 
 def playerTurn():
     playsCount = 1  
-  
-    if playsCount == 0:
-        playsCount += 1
-    else:
-        playsCount -= 1
+    winCheck = False
 
-    print(playerIndex[playsCount])
+    while winCheck  == False:
+        if playsCount == 0:
+            playsCount += 1
+        else:
+            playsCount -= 1
+
+        print(playerIndex[playsCount])
+
+        colChoice = input("choose column to place in    ")
+        colChoice = int(colChoice)
+        colChoice   -= 1
+        
+        for rowTest in range(5,-1,-1):
+            if board[rowTest][colChoice] == "_":
+                for i in range(0,rowTest):
+                    board[i][colChoice] = playerIndex[playsCount]
+                    displayBoard()
+                    time.sleep(0.25)
+                    board[i][colChoice] = "_"
+                    displayBoard()
+                board[rowTest][colChoice] = playerIndex[playsCount]
+                break
+            else:
+                continue
+            
+        
+        displayBoard()
 
 
-
-playerTurn()
 initialiseBoard()
 displayBoard()
-input()
+playerTurn()
