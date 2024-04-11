6ad687fd14eb4ed9287d5831a5f44f89d7840bbd
malachi12345678 <140124190+malachi12345678@users.noreply.github.com>
2024-04-12 09:46:04 +1000

    xfgmxbndgbnjdfhgdf

--- connect-four.py
+++ connect-four.py
@@ -2,6 +2,7 @@
 board = []
 playerIndex = {0 : "O" , 1 : "X"}
 import time
+import os
 
 def initialiseBoard():
     for cols in range(6):
@@ -17,7 +18,7 @@ def displayBoard():
             print(cols, end="|") 
         print("")
 
-def playerTurn():
+def play():
     playsCount = 1  
     winCheck = False
 
@@ -27,29 +28,53 @@ def playerTurn():
         else:
             playsCount -= 1
 
-        print(playerIndex[playsCount])
+        #print(playerIndex[playsCount])
 
         colChoice = input("choose column to place in    ")
         colChoice = int(colChoice)
         colChoice   -= 1
         
-        for rowTest in range(5,-1,-1):
-            if board[rowTest][colChoice] == "_":
-                for i in range(0,rowTest):
+        for rowChoice in range(5,-1,-1):
+            if board[rowChoice][colChoice] == "_":
+                for i in range(0,rowChoice):
                     board[i][colChoice] = playerIndex[playsCount]
+                    os.system('cls')
                     displayBoard()
                     time.sleep(0.25)
                     board[i][colChoice] = "_"
+                    os.system('cls')
                     displayBoard()
-                board[rowTest][colChoice] = playerIndex[playsCount]
+                board[rowChoice][colChoice] = playerIndex[playsCount]
                 break
             else:
                 continue
             
-        
+        os.system('cls')
         displayBoard()
+
+        #draw check
+        emptySpace = 0
+        for rowTest in range(6):
+            for colTest in range (5):
+                print(board[rowTest][colTest])
+                if board[rowTest][colTest] == "_":
+                    emptySpace +=1
+                else:
+                    continue
+        if emptySpace > 0:
+            winCheck = False
+        else:
+            print("draw")
+            winCheck = True
+
+
+
+
+
+
+
 
 
 initialiseBoard()
 displayBoard()
-playerTurn()
+play()
